819dd82a690b1e8fc8561bee1e5c884a8c9c45de
ljrGit1122 <97593178+ljrGit1122@users.noreply.github.com>
2022-02-08 22:36:08 +0800

    pull commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git 3!");
         System.out.println("hello git hot-fix!");
         System.out.println("hello git push master!");
+        System.out.println("hello git pull master!");
 
     }
 }
